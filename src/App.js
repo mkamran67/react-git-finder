1269878a2a3761814dd4ba5f0c2b767e0c3ff3e2
Muhammad Kamran <silverberry@Muhammads-MacBook-Pro.local>
2020-01-01 15:00:05 -0800

    Set up an alert for blank searches, and clear when there is a result output

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 
 import './App.css';
 import axios from 'axios';
@@ -9,26 +11,64 @@ import axios from 'axios';
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
-  async componentDidMount() {
+
+  // Searches on load for 30 users (default by Github API)
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${process.env.REACT_APP_USER_FINDER_ID}&client_secret=${process.env.REACT_APP_USER_FINDER_SECRET}`
+  //   );
+
+  //   // Runs after await
+  //   this.setState({ users: res.data, loading: false });
+  // }
+
+  // Search Github Users
+  searchUsers = async text => {
     this.setState({ loading: true });
 
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_USER_FINDER_ID}&client_secret=${process.env.REACT_APP_USER_FINDER_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_USER_FINDER_ID}&client_secret=${process.env.REACT_APP_USER_FINDER_SECRET}`
     );
 
     // Runs after await
-    this.setState({ users: res.data, loading: false });
-  }
+    this.setState({ users: res.data.items, loading: false });
+  };
 
-  // Render method is required, life cycle method
+  // CLear users from state
+  clearUsers = () => {
+    this.setState({
+      users: [],
+      loading: false
+    });
+  };
+
+  // set alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+  };
+
+  // Render method, life cycle method
   render() {
+    // Destructuring
+    const { users, loading } = this.state;
+
     return (
       <div className='App'>
         <Navbar title='Github Finder' githubIcon='fab fa-github' />
         <div className='container'>
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Alert alert={this.state.alert} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
